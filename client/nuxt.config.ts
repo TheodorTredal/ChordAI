49f4e4d61b0edcfcaa83a45145986e8f1196d85d
Theodor Tredal <ttr042@uit.no>
2026-05-29 11:43:22 +0200

    ChordAI can now utilize the GPU's on the IFI cluster

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -9,23 +9,6 @@ export default defineNuxtConfig({
     },
   },
 
-  // nitro: {
-  //   devProxy: {
-  //     '/api': {
-  //       target: 'http://localhost:5555/api',
-  //       changeOrigin: true,
-  //       prependPath: true
-  //     },
-  //     // Siden loggene dine viste "/ws/generate", har jeg inkludert denne for WebSockets:
-  //     '/ws': {
-  //       target: 'http://localhost:5555/ws',
-  //       ws: true,
-  //       changeOrigin: true
-  //     }
-  //   }
-  // }
-
-
   nitro: {
     devProxy: {
       '/api': {
@@ -42,47 +25,3 @@ export default defineNuxtConfig({
     }
   }
 })
-
-
-// nitro: {
-//     devProxy: {
-//       '/api': {
-//         target: 'http://c6-4:5555/api', // Sender nå til Go på GPU-noden
-//         changeOrigin: true,
-//         prependPath: false
-//       },
-//       '/ws': {
-//         target: 'http://c6-4:5555/ws',  // Sender nå til Go på GPU-noden
-//         ws: true,
-//         changeOrigin: true,
-//         prependPath: false
-//       }
-//     }
-//   }
-
-// export default defineNuxtConfig({
-//   compatibilityDate: '2025-07-15',
-//   devtools: { enabled: false },
-//   modules: ['@nuxtjs/tailwindcss'],
-  
-//   runtimeConfig: {
-//     public: {
-//       serverUrl: 'http://c6-4:8000', 
-//     },
-//   },
-
-//   nitro: {
-//     devProxy: {
-//       '/api': {
-//         target: 'http://c6-4:8000/api', // Trafikken rutes internt fra c0-0 til c6-4
-//         changeOrigin: true,
-//         prependPath: true
-//       },
-//       '/ws': {
-//         target: 'http://c6-4:8000/ws',
-//         ws: true,
-//         changeOrigin: true
-//       }
-//     }
-//   }
-// })
